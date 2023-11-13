78b952323aac7f07bff3cd83675ed4ea96bf364f
Stayhett <jona.laudan@gmail.com>
2023-11-13 21:55:22 +0100

    feature: Elastic Connector

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module GoBroke
 
 go 1.21
+
+require (
+	github.com/elastic/go-elasticsearch/v8 v8.11.0
+	github.com/joho/godotenv v1.5.1
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require github.com/elastic/elastic-transport-go/v8 v8.3.0 // indirect
---
+++ pkg/connector/elastic.go
@@ -0,0 +1,83 @@
+package connector
+
+import (
+	"bytes"
+	"context"
+	"crypto/tls"
+	"encoding/json"
+	"github.com/elastic/go-elasticsearch/v8"
+	"github.com/elastic/go-elasticsearch/v8/esutil"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+	"runtime"
+	"time"
+)
+
+func UploadToElastic() {
+	err := godotenv.Load("elastic.env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Upload to elastic
+	es, err := elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: []string{os.Getenv("ELASTICSEARCH_URL")},
+		Username:  os.Getenv("ELASTICSEARCH_USERNAME"),
+		Password:  os.Getenv("ELASTICSEARCH_PASSWORD"),
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+	})
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+
+	bi, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+		Client:        es,
+		Index:         "liviana",
+		NumWorkers:    runtime.NumCPU(), // The number of worker goroutines
+		FlushBytes:    int(5e+6),        // The flush threshold in bytes
+		FlushInterval: 30 * time.Second, // The periodic flush interval
+	})
+	if err != nil {
+		log.Fatalf("Error creating the indexer: %s", err)
+	}
+
+	var data []struct{}
+	for _, doc := range data {
+		data, err := json.Marshal(doc)
+		if err != nil {
+			log.Fatalf("Error marshalling document: %s", err)
+		}
+
+		err = bi.Add(
+			context.Background(),
+			esutil.BulkIndexerItem{
+				Index:  "liviana",
+				Body:   bytes.NewReader(data),
+				Action: "index",
+				OnFailure: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem, err error) {
+					if err != nil {
+						log.Printf("ERROR: %s", err)
+					} else {
+						log.Printf("ERROR: %s: %s", res.Error.Type, res.Error.Reason)
+					}
+				},
+			},
+		)
+		if err != nil {
+			log.Fatalf("Error adding document to the indexer: %s", err)
+		}
+	}
+
+	if err := bi.Close(context.Background()); err != nil {
+		log.Fatalf("Unexpected error: %s", err)
+	}
+	stats := bi.Stats()
+	if stats.NumFailed > 0 {
+		log.Fatalf("Indexed [%d] documents with [%d] errors", stats.NumFlushed, stats.NumFailed)
+	} else {
+		log.Printf("Successfully indexed [%d] documents", stats.NumFlushed)
+	}
+}
