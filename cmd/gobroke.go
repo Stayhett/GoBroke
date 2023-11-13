a95e825ca07448b0dabb8c1dc06c72f2d2877403
Stayhett <jona.laudan@gmail.com>
2023-11-13 21:54:45 +0100

    feature: configuration marshalling

--- cmd/gobroke.go
+++ cmd/gobroke.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"GoBroke/pkg/broker"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
+func main() {
+	path := "configuration/"
+
+	dir, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dir.Close()
+
+	// Read the contents of the directory
+	fileInfos, err := dir.Readdir(-1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var configurations []broker.Configuration
+	for _, fileInfo := range fileInfos {
+		if strings.HasSuffix(fileInfo.Name(), ".yml") {
+			err, c := broker.ConstructConfigurationFromFile(path + fileInfo.Name())
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			configurations = append(configurations, c)
+		}
+	}
+
+	fmt.Println(configurations)
 }
