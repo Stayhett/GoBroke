2be492bca5e6b3877e54c90fae3746027b3d2fea
Stayhett <jona.laudan@gmail.com>
2023-12-15 19:49:47 +0100

    chore: elastic clean up

--- pkg/broker/elastic.go
+++ pkg/broker/elastic.go
@@ -14,49 +14,47 @@ import (
 	"time"
 )
 
+const (
+	defaultFlushBytes = int(5e+6)
+)
+
 func UploadToElasticHandler(index string, data []map[string]interface{}, output Output) {
-	if output.Key != "" {
-		UploadToElastic(index, data, &elasticsearch.Config{
-			Addresses: []string{os.Getenv(output.Host)},
-			APIKey:    os.Getenv(output.Key),
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			},
-		})
-	} else {
-		UploadToElastic(index, data, &elasticsearch.Config{
-			Addresses: []string{os.Getenv(output.Host)},
-			Username:  os.Getenv(output.Username),
-			Password:  os.Getenv(output.Password),
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-			},
-		})
+	esConfig := &elasticsearch.Config{
+		Addresses: []string{os.Getenv(output.Host)},
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
 	}
+
+	if output.Key != "" {
+		esConfig.APIKey = os.Getenv(output.Key)
+	} else {
+		esConfig.Username = os.Getenv(output.Username)
+		esConfig.Password = os.Getenv(output.Password)
+	}
+
+	UploadToElastic(index, data, esConfig)
 }
 
 func UploadToElastic(index string, data []map[string]interface{}, esConfig *elasticsearch.Config) {
 	// Upload to elastic
 	es, err := elasticsearch.NewClient(*esConfig)
 	if err != nil {
-		log.Fatalf("Error creating the client: %s", err)
+		log.Printf("error creating the client: %s", err)
+		return
 	}
 
-	bi, err := esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
-		Client:        es,
-		Index:         index,
-		NumWorkers:    runtime.NumCPU(),
-		FlushBytes:    int(5e+6),
-		FlushInterval: 30 * time.Second,
-	})
+	bi, err := configureBulkIndexer(es, index)
 	if err != nil {
-		log.Fatalf("Error creating the indexer: %s", err)
+		log.Printf("error creating the indexer: %s", err)
+		return
 	}
 
 	for _, doc := range data {
 		data, err := json.Marshal(doc)
 		if err != nil {
-			log.Fatalf("Error marshalling document: %s", err)
+			log.Printf("error marshalling document: %s", err)
+			continue
 		}
 
 		err = bi.Add(
@@ -67,25 +65,37 @@ func UploadToElastic(index string, data []map[string]interface{}, esConfig *elas
 				Action: "index",
 				OnFailure: func(ctx context.Context, item esutil.BulkIndexerItem, res esutil.BulkIndexerResponseItem, err error) {
 					if err != nil {
-						log.Printf("ERROR: %s", err)
+						log.Printf("error adding document to the indexer: %s", err)
 					} else {
-						log.Printf("ERROR: %s: %s", res.Error.Type, res.Error.Reason)
+						log.Printf("error: %s: %s", res.Error.Type, res.Error.Reason)
 					}
 				},
 			},
 		)
 		if err != nil {
-			log.Fatalf("Error adding document to the indexer: %s", err)
+			log.Printf("error adding document to the indexer: %s", err)
 		}
 	}
 
 	if err := bi.Close(context.Background()); err != nil {
-		log.Fatalf("Unexpected error: %s", err)
+		log.Printf("Unexpected error: %s", err)
+		return
 	}
+
 	stats := bi.Stats()
 	if stats.NumFailed > 0 {
-		log.Fatalf("Indexed [%d] documents with [%d] errors", stats.NumFlushed, stats.NumFailed)
+		log.Printf("Indexed [%d] documents with [%d] errors", stats.NumFlushed, stats.NumFailed)
 	} else {
 		log.Printf("Successfully indexed [%d] documents", stats.NumFlushed)
 	}
+}
+
+func configureBulkIndexer(es *elasticsearch.Client, index string) (esutil.BulkIndexer, error) {
+	return esutil.NewBulkIndexer(esutil.BulkIndexerConfig{
+		Client:        es,
+		Index:         index,
+		NumWorkers:    runtime.NumCPU(),
+		FlushBytes:    defaultFlushBytes,
+		FlushInterval: 30 * time.Second,
+	})
 }
