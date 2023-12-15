46bde678f9f06f00266dc45b82c017ebe427e8cf
Stayhett <jona.laudan@gmail.com>
2023-12-15 20:35:55 +0100

    feature: clean up

--- pkg/broker/input.go
+++ pkg/broker/input.go
@@ -1,6 +1,10 @@
 package broker
 
-import "errors"
+import (
+	"errors"
+	"io"
+	"net/http"
+)
 
 func InputHandler(inputConfig *Input, location string) ([]byte, error) {
 	switch inputConfig.Connector {
@@ -9,4 +13,17 @@ func InputHandler(inputConfig *Input, location string) ([]byte, error) {
 	default:
 		return nil, errors.New("error unknown input")
 	}
+}
+
+func fetchData(url string) ([]byte, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+	return body, nil
 }
